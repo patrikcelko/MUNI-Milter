3a255e8dcf4def3c61f35f0d364d5f7a5e7bb4f3
patrikcelko <patrik@celko.cz>
2022-12-04 21:56:41 +0100

    Fix typo in database.h

--- src/libs/database.h
+++ src/libs/database.h
@@ -1,25 +1,25 @@
-/*************************************************************************************
-* Copyright [2022] [Patrik Čelko]
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
-* file except in compliance with the License. You may obtain a copy of the License at 
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software distributed under 
-* the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
-* KIND, either express or implied. See the License for the specific language governing 
-* permissions and limitations under the License.
-*
-***************************************************************************************/
+/*****************************************************************************************
+ * Copyright [2022] [Patrik Čelko]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ *****************************************************************************************/
 
-/********************************** MUNI - Milter ***********************************
+/********************************** MUNI - Milter *****************************************
  *
  * FILENAME:    database.h
- * DESCRIPTION: Header file for the database for milter.
+ * DESCRIPTION: Header file for the database for Milter.
  * NOTES:       This lib is part of the MUNI-Milter, and will not work on its own.
  * AUTHOR:      Patrik Čelko
  *
- *************************************************************************************/
+ *****************************************************************************************/
 
 #ifndef DATABASE_H
 #define DATABASE_H
