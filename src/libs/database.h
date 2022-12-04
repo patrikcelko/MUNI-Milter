8317c49fdd4bfb5a49dc0c0ef4d0d5eeba2c1b66
patrikcelko <patrik@celko.cz>
2022-12-04 21:53:08 +0100

    Update database header & add license

--- src/libs/database.h
+++ src/libs/database.h
@@ -1,3 +1,17 @@
+/*************************************************************************************
+* Copyright [2022] [Patrik Čelko]
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
+* file except in compliance with the License. You may obtain a copy of the License at 
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under 
+* the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+* KIND, either express or implied. See the License for the specific language governing 
+* permissions and limitations under the License.
+*
+***************************************************************************************/
+
 /********************************** MUNI - Milter ***********************************
  *
  * FILENAME:    database.h
@@ -14,10 +28,12 @@
 
 /* Database entry/bucket structure */
 struct entry {
-    char* key;
-    time_t last_email;
-    int score;
-    struct entry* next;
+    char* key; // The hash value of the sender
+    time_t last_email; // The time when the last email was sent
+    int email_count; // The total amount of emails for the selected sender
+    float average_time; // The average time between sending emails for the selected sender
+    float average_score; // Average spam score for the selected sender
+    struct entry* next; // Next entry if the hash match
 };
 
 /* Database structure */
@@ -29,18 +45,8 @@ struct database {
     int entry_counter;
 };
 
-/* Additional email info structure for function 'db_new_score' */
-struct email_info {
-    int assassin_score;
-    time_t last_email;
-    bool is_auth;
-    bool is_forward;
-    bool is_local;
-};
-
 typedef struct entry entry_t;
 typedef struct database database_t;
-typedef struct email_info email_info_t;
 
 /* [Thread-Unsafe] Construct database structure */
 database_t* db_construct(pthread_mutex_t mutex_value, settings_t* settings);
@@ -48,22 +54,16 @@ database_t* db_construct(pthread_mutex_t mutex_value, settings_t* settings);
 /* [Thread-Unsafe] Destroy database structure */
 void db_destroy(database_t* db_instance);
 
-/* [Thread-Safe] Set entry in database */
-void db_set(database_t* db_instance, char* key, time_t last_email, int score);
-
-/* [Thread-Safe] Get entry from database */
+/* [Thread-Safe] Get an entry from the database (if does not exist create a new one) */
 entry_t* db_get(database_t* db_instance, char* ip_to_search);
 
-/* [Thread-Unsafe] Save whole database to file */
+/* [Thread-Unsafe] Save the whole database to file */
 void db_save(database_t* db_instance);
 
-/* [Thread-Unsafe] Load database from local path */
+/* [Thread-Unsafe] Load database from local path/file */
 void db_load(database_t* db_instance);
 
-/* [Thread-Safe] Remove old records from database and update score */
+/* [Thread-Safe] Remove old records from the database and update the score */
 void db_cleanup(database_t* db_instance);
-
-/* [Thread-Safe] Calculate new score for database */
-int db_new_score(int old_score, email_info_t* email_info, settings_t* settings);
 
 #endif
