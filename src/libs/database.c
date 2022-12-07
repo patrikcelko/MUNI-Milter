25174dfa176f1ce72f6a5677a81ff853a1d93ae0
patrikcelko <patrik@celko.cz>
2022-12-07 14:47:48 +0100

    Refactor, fix some leaks, Split parser

--- src/libs/database.c
+++ src/libs/database.c
@@ -12,7 +12,7 @@
  *
  *****************************************************************************************/
 
-/********************************** MUNI - Milter *****************************************
+/********************************** MUNI - Milter ****************************************
  *
  * FILENAME:	database.c
  * DESCRIPTION:	Implementation of the database for Milter.
@@ -72,6 +72,7 @@ database_t* db_construct(pthread_mutex_t mutex_value, settings_t* settings)
     db_instance->data = malloc(sizeof(entry_t*) * settings->hash_table_size);
     if (!db_instance->data) {
         syslog(LOG_ERR, "Was not able to allocate memory for the database hash table.");
+        free(db_instance); // Reclaim struct memory
         return NULL;
     }
 
@@ -145,6 +146,7 @@ entry_t* create_new_entry(char* key, database_t* db_instance)
     entry->key = malloc(strlen(key) + 1);
     if (!entry->key) {
         syslog(LOG_ERR, "Was not able to allocate memory for the key value. Key: %s.", key);
+        free(entry);
         return NULL;
     }
 
@@ -261,6 +263,55 @@ void db_save(database_t* db_instance)
     }
 }
 
+/* [Thread-Unsafe] Save one line from the file to the database if valid */
+bool save_entry_line(char *line_content, database_t *db_instance) 
+{
+    char* key = strtok(line_content, DATABASE_DELIMITER);
+    char* raw_email_count = strtok(NULL, DATABASE_DELIMITER);
+    char* raw_average_time = strtok(NULL, DATABASE_DELIMITER);
+    char* raw_average_score = strtok(NULL, DATABASE_DELIMITER);
+
+    if (!key || !raw_email_count || !raw_average_time || !raw_average_score) {
+        return false; // Invalid line in saved database
+    }
+
+    if (is_whitelisted(key, db_instance->settings_instance)) {
+        syslog(LOG_DEBUG, "[db_load] IP %s was removed from records because is whitelisted.", key);
+        return false;
+    }
+    errno = 0;
+
+    // Parse email_count as integer
+    char* email_count_end;
+    int email_count = strtol(raw_email_count, &email_count_end, 10);
+
+    // Parse average_time as float
+    char* average_time_end;
+    int average_time = strtof(raw_average_time, &average_time_end);
+
+    // Parse average_score as float
+    char* average_score_end;
+    int average_score = strtof(raw_average_score, &average_score_end);
+
+    if (errno != 0 || email_count_end == raw_email_count || average_time_end == raw_average_time || average_score_end == raw_average_score) {
+        syslog(LOG_WARNING, "Was not able to load the email_conut, average_time, or average_score from the saved database key: %s. Skipping.", key);
+        return false; // Invalid line in database, skipping (parsing error)
+    }
+
+    entry_t* entry = db_get(db_instance, key);
+    if (!entry) {
+        syslog(LOG_WARNING, "Was not able to get an entry for %s, during loading from file.", key);
+        return false;
+    }
+
+    entry->last_email = time(0);
+    entry->email_count = email_count;
+    entry->average_time = average_time;
+    entry->average_score = average_score;
+
+    return true;
+}
+
 /* [Thread-Unsafe] Load database from local path/file */
 void db_load(database_t* db_instance)
 {
@@ -283,49 +334,9 @@ void db_load(database_t* db_instance)
     size_t data_length;
 
     while (getline(&line_content, &data_length, db_load_fd) != EOF) {
-        char* key = strtok(line_content, DATABASE_DELIMITER);
-        char* raw_email_count = strtok(NULL, DATABASE_DELIMITER);
-        char* raw_average_time = strtok(NULL, DATABASE_DELIMITER);
-        char* raw_average_score = strtok(NULL, DATABASE_DELIMITER);
-
-        if (!key || !raw_email_count || !raw_average_time || !raw_average_score) {
-            continue; // Invalid line in saved database, skipping (missing part)
+        if(save_entry_line(line_content, db_instance)) {
+            loaded_counter++;
         }
-
-        if (is_whitelisted(key, db_instance->settings_instance)) {
-            syslog(LOG_DEBUG, "[db_load] IP %s was removed from records because is whitelisted.", key);
-            continue;
-        }
-
-        loaded_counter++;
-        errno = 0;
-
-        // Parse email_count as integer
-        char* email_count_end;
-        int email_count = strtol(raw_email_count, &email_count_end, 10);
-
-        // Parse average_time as float
-        char* average_time_end;
-        int average_time = strtof(raw_average_time, &average_time_end);
-
-        // Parse average_score as float
-        char* average_score_end;
-        int average_score = strtof(raw_average_score, &average_score_end);
-
-        if (errno != 0 || email_count_end == raw_email_count || average_time_end == raw_average_time || average_score_end == raw_average_score) {
-            syslog(LOG_WARNING, "Was not able to load the email_conut, average_time, or average_score from the saved database key: %s. Skipping.", key);
-            continue; // Invalid line in database, skipping (parsing error)
-        }
-
-        entry_t* entry = db_get(db_instance, key);
-        if (!entry) {
-            syslog(LOG_WARNING, "Was not able to get an entry for %s, during loading from file.", key);
-        }
-
-        entry->last_email = time(0);
-        entry->email_count = email_count;
-        entry->average_time = average_time;
-        entry->average_score = average_score;
     }
 
     if (line_content) {
@@ -363,16 +374,22 @@ void db_cleanup_recursion(entry_t* entry, entry_t* prev_entry, database_t* db_in
     }
 
     pthread_mutex_lock(&db_instance->mutex_value);
+    
     free(entry->key);
-
     db_instance->entry_counter--;
-    entry_t* tmp_prev_entry = !prev_entry ? db_instance->data[record_index] : prev_entry->next;
-    tmp_prev_entry = entry->next;
-
-    free(entry);
+    
+    if (!prev_entry) {
+        db_instance->data[record_index] = entry->next;
+    } else {
+        prev_entry->next = entry->next;
+    }
     pthread_mutex_unlock(&db_instance->mutex_value);
 
-    db_cleanup_recursion(tmp_prev_entry, !prev_entry ? db_instance->data[record_index] : prev_entry, db_instance, record_index, actual_counter);
+    db_cleanup_recursion(entry->next, !prev_entry ? db_instance->data[record_index] : prev_entry, db_instance, record_index, actual_counter);
+
+    pthread_mutex_lock(&db_instance->mutex_value);
+    free(entry);
+    pthread_mutex_unlock(&db_instance->mutex_value);
 }
 
 /* [Thread-Safe] Cleanup thread */
